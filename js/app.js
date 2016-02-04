8392b80e3a0683432ac6a17b6de08905e3672300
Parisot Joffrey <joffrey.parisot@webdistrib.com>
2016-02-04 13:13:08 +0100

    Ajout JS animation

--- js/app.js
+++ js/app.js
@@ -25,8 +25,47 @@ $('.realm__link').on('click', function(e){
 
     $('body').addClass('view-realm');
 
-    var target = $(this).attr('href').split('#')[1];
+    var target = $(this).attr('href');
     console.log(target);
 
-    $('.' + target).addClass('realm__content--show');
+    $(target).addClass('realm__content--show');
 });
+
+
+var bodyEl = document.body,
+    docElem = window.document.documentElement,
+    realmLeft = document.getElementById('realm--left'),
+    realmRight = document.getElementById('realm--right');
+
+/**
+ * gets the viewport width and height
+ * based on http://responsejs.com/labs/dimensions/
+ */
+function getViewport( axis ) {
+    var client, inner;
+    if( axis === 'x' ) {
+        client = docElem['clientWidth'];
+        inner = window['innerWidth'];
+    }
+    else if( axis === 'y' ) {
+        client = docElem['clientHeight'];
+        inner = window['innerHeight'];
+    }
+
+    return client < inner ? inner : client;
+}
+
+function loadContent(item){
+    // add expanding element/placeholder
+    var dummy = document.createElement('div');
+    dummy.className = 'placeholder';
+
+    // set the width/heigth and position
+    dummy.style.transform = 'translate3d(' + (item.offsetLeft - 5) + 'px, ' + (item.offsetTop - 5) + 'px, 0px) scale3d(' + item.offsetWidth/(realmLeft.offsetWidth + realmRight.offsetWidth) + ',' + item.offsetHeight/getViewport('y') + ',1)';
+
+    // add transition class
+    $('.placeholder').addClass('placeholder--trans-in');
+
+    // insert it after all the grid items
+    document.getElementById('main').appendChild(dummy);
+}
